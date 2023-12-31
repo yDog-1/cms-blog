8cd8778f92701beb8ff1dc34cc301ca472bdf6e1
yDog-1 <kokkoh@outlook.jp>
2023-12-31 14:20:14 +0900

    ページ一覧を表示

---
+++ app/post/[postId]/page.tsx
@@ -0,0 +1,30 @@
+import { getDetail, getList } from "@/libs/microcms";
+import { notFound } from "next/navigation";
+import parse from "html-react-parser";
+
+export async function generateStaticParams() {
+  const { contents } = await getList();
+
+  const paths = contents.map((post) => {
+    return {
+      postId: post.id,
+    };
+  });
+
+  return [...paths];
+}
+
+export default async function StaticDetailPage({
+  params: { postId },
+}: {
+  params: { postId: string };
+}) {
+  const post = await getDetail(postId);
+
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <div>{parse(post.body)}</div>
+    </div>
+  );
+}
