c7cb873b65972128370b6ba7ea726dd6a70599b6
yDog-1 <kokkoh@outlook.jp>
2024-01-04 17:58:56 +0900

    微修正

--- app/post/[postId]/loading.tsx
+++ app/post/[postId]/loading.tsx
@@ -1,3 +1,7 @@
 export default function Loading() {
-  return <h1 className="container mx-auto md:w-2/3">Loading...</h1>;
+  return (
+    <main className="container mx-auto flex-1 md:w-2/3">
+      <h1>Loading...</h1>
+    </main>
+  );
 }
