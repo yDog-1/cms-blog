0c0d37c09c4302d1a27040f5216435abe226bb92
yDog-1 <kokkoh@outlook.jp>
2024-01-04 17:58:42 +0900

    フッターが下に行くように

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="container mx-auto md:w-2/3">
+    <div className="container mx-auto flex-1 md:w-2/3">
       <h2>Not Found</h2>
       <p>Could not find requested resource</p>
       <Link className=" text-blue-500 hover:underline" href="/">
