ed62d75f78365299b0bc644a3d4b17a14d66ab6c
yDog-1 <kokkoh@outlook.jp>
2024-01-04 17:01:34 +0900

    not-found画面を仮実装

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="container mx-auto md:w-2/3">
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+      <Link className=" text-blue-500 hover:underline" href="/">
+        Return Home
+      </Link>
+    </div>
+  );
+}
