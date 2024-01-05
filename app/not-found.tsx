b9d762b80a1358be782150effcd8e94051af36cd
yDog-1 <kokkoh@outlook.jp>
2024-01-05 18:20:06 +0900

    scssのリファクタリング忘れ

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="container mx-auto flex-1 md:w-2/3">
+    <main>
       <h2>Not Found</h2>
       <p>Could not find requested resource</p>
       <Link className=" text-blue-500 hover:underline" href="/">
         Return Home
       </Link>
-    </div>
+    </main>
   );
 }
--- app/post/[postId]/loading.tsx
+++ app/post/[postId]/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <main className="container mx-auto flex-1 md:w-2/3">
+    <main>
       <h1>Loading...</h1>
     </main>
   );
