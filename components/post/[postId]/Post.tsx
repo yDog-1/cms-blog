603727199800e5e3eb605d20a11b3e9f8d411daf
yDog-1 <kokkoh@outlook.jp>
2024-01-06 00:35:27 +0900

    コンパイル順の関係で別ファイルでclassNameを返すとTailwindcssが適用されない

--- components/post/[postId]/Post.tsx
+++ components/post/[postId]/Post.tsx
@@ -2,7 +2,29 @@ import { getDetail, getList } from "@/libs/microcms";
 import parse from "html-react-parser";
 import styles from "@/components/post/[postId]/Post.module.scss";
 import Link from "next/link";
-import getTagColor from "@/libs/getTagColor";
+import { Tag } from "@/types/Blog";
+
+function getTagColor(tag: Tag) {
+  const lTex = "text-slate-50";
+  switch (tag) {
+    case "Next.js":
+      return `bg-slate-800 ${lTex}`;
+    case "Javascript":
+      return `bg-yellow-500`;
+    case "Typescript":
+      return `bg-blue-500 ${lTex}`;
+    case "microCMS":
+      return `bg-slate-700 ${lTex}`;
+    case "tailwindcss":
+      return `bg-sky-500 ${lTex}`;
+    case "Notion":
+      return `bg-slate-900 ${lTex}`;
+    case "scss":
+      return `bg-fuchsia-400 ${lTex}`;
+    default:
+      return "background-color:red;";
+  }
+}
 
 export async function generateStaticParams() {
   const contents = await getList();
