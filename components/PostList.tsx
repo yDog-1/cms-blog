9c2a9e2f1589c2354d5c6e7bdf4f516f61b1d744
yDog-1 <kokkoh@outlook.jp>
2024-01-05 18:16:14 +0900

    最新の投稿だけ切り出す

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -5,35 +5,56 @@ import { FaArrowCircleRight } from "react-icons/fa";
 
 export default async function PostList() {
   const contents = await getList();
-
   if (!contents || contents.length === 0) {
     return notFound();
   }
+  const firstContent = contents[0];
+  const otherContents = contents.slice(1);
   return (
-    <ul className="my-10 grid grid-cols-1 gap-3 md:grid-cols-3">
-      {contents.map((post) => (
-        <li
-          key={post.id}
-          className="flex rounded-lg bg-slate-50 shadow-lg shadow-slate-300 duration-100 active:shadow-none md:mx-0"
+    <>
+      <div className="mt-10 flex rounded-lg bg-slate-50 shadow-lg shadow-slate-300 duration-100 active:shadow-none md:mx-0">
+        <Link
+          href={`/post/${firstContent.id}`}
+          className="group flex flex-1 flex-col"
         >
-          <Link
-            href={`/post/${post.id}`}
-            className="group flex flex-1 flex-col py-1.5"
+          <h2 className="px-5 pb-10 pt-3 text-2xl font-bold group-hover:underline">
+            {firstContent.title}
+          </h2>
+          <div className="mt-auto flex justify-between px-5">
+            <p className="py-3 text-sm">{firstContent.localPublishedAt}</p>
+            <FaArrowCircleRight
+              color={"#DC2626"}
+              size={"1.7em"}
+              className="my-auto ml-auto"
+            />
+          </div>
+        </Link>
+      </div>
+      <ul className="mb-10 mt-6 grid grid-cols-1 gap-3 md:grid-cols-3">
+        {otherContents.map((post) => (
+          <li
+            key={post.id}
+            className="flex rounded-lg bg-slate-50 shadow-lg shadow-slate-300 duration-100 active:shadow-none md:mx-0"
           >
-            <h2 className="px-5 py-3 text-2xl font-bold group-hover:underline">
-              {post.title}
-            </h2>
-            <div className="mt-auto flex justify-between px-5">
-              <p className="py-3 text-sm">{post.localPublisheAt}</p>
-              <FaArrowCircleRight
-                color={"#DC2626"}
-                size={"1.7em"}
-                className="my-auto ml-auto"
-              />
-            </div>
-          </Link>
-        </li>
-      ))}
-    </ul>
+            <Link
+              href={`/post/${post.id}`}
+              className="group flex flex-1 flex-col"
+            >
+              <h2 className="px-5 pb-10 pt-3 text-2xl font-bold group-hover:underline">
+                {post.title}
+              </h2>
+              <div className="mt-auto flex justify-between px-5">
+                <p className="py-3 text-sm">{post.localPublishedAt}</p>
+                <FaArrowCircleRight
+                  color={"#DC2626"}
+                  size={"1.7em"}
+                  className="my-auto ml-auto"
+                />
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
