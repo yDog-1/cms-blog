6f6eb43074dada25cb75b2b1c643e0993adfe7a6
yDog-1 <kokkoh@outlook.jp>
2024-01-07 00:28:37 +0900

    外部画像をImageで表示するための設定を追加

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.microcms-assets.io",
+        port: "",
+        pathname: "/assets/78e4da89fd5b4ae1a8d1a56151a2875e/**/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
