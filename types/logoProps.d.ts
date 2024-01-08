0c9e5541dc7890cacbd7fecdeb56d28a8846a505
yDog-1 <kokkoh@outlook.jp>
2024-01-08 14:01:35 +0900

    ディレクトリ整頓

--- types/logoProps.d.ts
+++
@@ -1,9 +0,0 @@
-class SuperLogoProps {
-  width: number;
-  height: number;
-  darkMode: Boolean;
-  Dir: { black: string; white: string };
-  Name: string;
-}
-
-type LogoProps = Omit<SuperLogoProps, "Dir" | "Name">;
