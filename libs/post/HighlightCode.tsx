fe69c034ffee0a71de8d0995c5c5cf2a31b3fe1d
yDog-1 <kokkoh@outlook.jp>
2024-02-10 21:35:13 +0900

    非推奨コードを修正

--- libs/post/HighlightCode.tsx
+++ libs/post/HighlightCode.tsx
@@ -11,7 +11,10 @@ export default function HighlightCode({
   hlc: { code, languageClass, dataFileName },
 }: Props) {
   const language = languageClass.replace("language-", "");
-  const highlightCode = hljs.highlight(language, code).value;
+  const highlightCode = hljs.highlight(code, {
+    language: language,
+    ignoreIllegals: true,
+  }).value;
   return (
     <div className={styles.codeBlock}>
       <div className={styles.dataFileName}>{dataFileName}</div>
