0214707d67837cd11bfacacd8db349bd2ab90703
yDog-1 <kokkoh@outlook.jp>
2024-02-10 23:55:33 +0900

    commonに戻した

--- libs/post/highlightjs/HighlightCode.tsx
+++ libs/post/highlightjs/HighlightCode.tsx
@@ -1,18 +1,7 @@
 import parse from "html-react-parser";
-import hljs from "highlight.js/lib/core";
+import hljs from "highlight.js/lib/common";
 import styles from "./HighlightCode.module.scss";
 import "highlight.js/styles/lioshi.css";
-
-hljs.registerLanguage(
-  "typescript",
-  require("highlight.js/lib/languages/typescript")
-);
-hljs.registerLanguage(
-  "javascript",
-  require("highlight.js/lib/languages/javascript")
-);
-hljs.registerLanguage("css", require("highlight.js/lib/languages/css"));
-hljs.registerLanguage("scss", require("highlight.js/lib/languages/scss"));
 
 type Props = {
   hlc: { code: string; languageClass: string; dataFileName: string };
