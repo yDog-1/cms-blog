2f2787854e2c129d18d5d568ac3ddac03453940d
yDog-1 <kokkoh@outlook.jp>
2024-02-10 17:35:45 +0900

    リファクタリング

--- app/tag/[tagName]/page.tsx
+++ app/tag/[tagName]/page.tsx
@@ -1,11 +1,11 @@
-import TagPage from "@/components/app/tag/[tagName]/TagPageList";
+import TagPage from "@/features/components/TagPageList/TagPageList";
 import { Tag } from "@/types/Tag";
 
 type Props = {
-  tagName: Tag;
+  params: { tagName: Tag };
 };
 
-export default async function Page({ tagName }: Props) {
+export default async function Page({ params: { tagName } }: Props) {
   return (
     <main className="flex flex-col">
       <TagPage tagName={tagName} />
