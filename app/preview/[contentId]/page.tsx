79c075ff0a77873dd41d236bbda366a8d64afe40
yDog-1 <kokkoh@outlook.jp>
2024-02-10 23:52:04 +0900

    実験

--- app/preview/[contentId]/page.tsx
+++ app/preview/[contentId]/page.tsx
@@ -3,7 +3,7 @@ import PreviewPage from "@/features/components/PreviewPage/PreviewPage";
 export default async function Page() {
   return (
     <main className=" flex flex-col items-center bg-white ">
-      <PreviewPage />
+      {/* <PreviewPage /> */}
     </main>
   );
 }
