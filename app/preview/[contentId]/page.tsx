70d67700195351d1db87557c93fbc770c368a3bd
yDog-1 <kokkoh@outlook.jp>
2024-02-24 15:02:27 +0900

    不要なmain要素を削除

--- app/preview/[contentId]/page.tsx
+++ app/preview/[contentId]/page.tsx
@@ -1,9 +1,5 @@
 import PreviewPage from "@/features/components/PreviewPage/PreviewPage";
 
 export default async function Page() {
-  return (
-    <main className=" flex flex-col items-center bg-white ">
-      <PreviewPage />
-    </main>
-  );
+  return <PreviewPage />;
 }
