229544daafa24bcc767283f09c78196cb02e5d4d
yDog-1 <kokkoh@outlook.jp>
2024-02-24 14:02:33 +0900

    埋め込み用useScriptの定義

---
+++ libs/post/script/Iframely.tsx
@@ -0,0 +1,8 @@
+"use client";
+import useScript from "./useScript";
+
+export default function Iframely() {
+  useScript("https://cdn.iframe.ly/embed.js");
+
+  return <></>;
+}
---
+++ libs/post/script/Twitter.tsx
@@ -0,0 +1,7 @@
+"use client";
+import useScript from "./useScript";
+
+export default function Twitter() {
+  useScript("https://platform.twitter.com/widgets.js");
+  return <></>;
+}
